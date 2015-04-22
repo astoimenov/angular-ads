bf0684121b36b4dd3d8ace56cb521b15576ecd03
Alexander Stoimenov <alexander.stoimenov@outlook.com>
2015-04-22 12:11:25 +0300

    Update bower.json

--- app/js/directives/di-href.js
+++ app/js/directives/di-href.js
@@ -1,14 +1,14 @@
-'use strict';
-
 adsApp.directive('diHref', ['$location', '$route',
     function ($location, $route) {
         return function (scope, element, attrs) {
             scope.$watch('diHref', function () {
                 if (attrs.diHref) {
                     element.attr('href', attrs.diHref);
-                    element.bind('click', function (event) {
+                    element.bind('click', function () {
                         scope.$apply(function () {
-                            if ('#' + $location.path() == attrs.diHref) $route.reload();
+                            if ('#' + $location.path() == attrs.diHref) {
+                                $route.reload();
+                            }
                         });
                     });
                 }
